b690d60b68d13a71e73e84c438f0d92a6a41fc28
ParkJH106 <jeongha4209@naver.com>
2021-03-30 10:50:52 +0900

    inner class without this

--- app/src/main/java/kr/ac/smu/smart0356/buttonwiinnerclass/MainActivity.java
+++ app/src/main/java/kr/ac/smu/smart0356/buttonwiinnerclass/MainActivity.java
@@ -3,6 +3,7 @@ package kr.ac.smu.smart0356.buttonwiinnerclass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -18,6 +19,13 @@ public class MainActivity extends AppCompatActivity {
         mButton1 = (Button) findViewById(R.id.button1);
         mTextView1 = (TextView) findViewById(R.id.textView1);
 
-        mButton1.setOnClickListener(new MyOnClickListener(this));
+        mButton1.setOnClickListener(new MyOnClickListener());
+    }
+    private class MyOnClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            mTextView1.setText("you clicked 박정하의 button!");
+        }
     }
 }
